c67d9165ac437b5f7e08fd91b3b39be21f813175
Sunki Baek <sunki.baek@gmail.com>
2019-04-28 05:50:39 -0700

    Add some solutions

--- 1_introduction/P0/Task1.py
+++ 1_introduction/P0/Task1.py
@@ -14,7 +14,19 @@ with open('calls.csv', 'r') as f:
 
 """
 TASK 1:
-How many different telephone numbers are there in the records? 
+How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+numbers = {}
+
+for item in texts:
+    numbers[item[0]] = True
+    numbers[item[1]] = True
+
+for item in calls:
+    numbers[item[0]] = True
+    numbers[item[1]] = True
+
+print(f'There are {len(numbers)} different telephone numbers in the records.')
--- 1_introduction/P0/Task2.py
+++ 1_introduction/P0/Task2.py
@@ -16,7 +16,28 @@ TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
 also time spent on the phone.
 Print a message:
-"<telephone number> spent the longest time, <total time> seconds, on the phone during 
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
 September 2016.".
 """
 
+durations = {}
+longest = 0
+longest_number = ""
+
+for item in calls:
+    (calling_number, receiving_number, at_time, duration) = item
+
+    duration = int(duration)
+
+    durations[calling_number] = durations.get(calling_number, 0) + duration
+    durations[receiving_number] = durations.get(receiving_number, 0) + duration
+
+    if durations[calling_number] > longest:
+        longest = durations[calling_number]
+        longest_number = calling_number
+
+    if durations[receiving_number] > longest:
+        longest = durations[receiving_number]
+        longest_number = receiving_number
+
+print(f'{longest_number} spent the longest time, {longest} seconds, on the phone during September 2016.')
