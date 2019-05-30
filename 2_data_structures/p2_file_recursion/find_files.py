2e2af6cdd628bdf874c07f20d881b96a5530eb0f
Sunki Baek <sunki.baek@gmail.com>
2019-05-29 21:11:53 -0700

    Add tests and explanation

--- 2_data_structures/p2_file_recursion/find_files.py
+++ 2_data_structures/p2_file_recursion/find_files.py
@@ -2,21 +2,6 @@ import os
 
 
 def find_files(suffix, path):
-    """
-    Find all files beneath path with file name suffix.
-
-    Note that a path may contain further subdirectories
-    and those subdirectories may also contain further subdirectories.
-
-    There are no limit to the depth of the subdirectories can be.
-
-    Args:
-      suffix(str): suffix if the file name to be found
-      path(str): path of the file system
-
-    Returns:
-       a list of paths
-    """
     dirs = os.listdir(path)
     match_file_paths = []
 
@@ -32,8 +17,11 @@ def find_files(suffix, path):
     return match_file_paths
 
 
-def main():
-    print(find_files(".c", "./testdir"))
+print(find_files(".c", "./testdir"))
+# ['./testdir/subdir3/subsubdir1/b.c', './testdir/t1.c', './testdir/subdir5/a.c', './testdir/subdir1/a.c']
 
+print(find_files(".h", "./testdir"))
+# ['./testdir/subdir3/subsubdir1/b.h', './testdir/subdir5/a.h', './testdir/t1.h', './testdir/subdir1/a.h']
 
-main()
+print(find_files(".py", "./"))
+# ['./find_files.py']
---
+++ 2_data_structures/readme.md
@@ -0,0 +1,3 @@
+In this problem, the challenge was to recursively search through the directories
+and sub directories. To achive this, recursive function has been used and
+the result python lists were concatenated.
