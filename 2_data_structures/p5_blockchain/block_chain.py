9886593a8b1785d90988f3fa5150f7210c8a4f07
Sunki Baek <sunki.baek@gmail.com>
2019-05-30 05:54:45 -0700

    Add tests and description

--- 2_data_structures/p5_blockchain/block_chain.py
+++ 2_data_structures/p5_blockchain/block_chain.py
@@ -20,14 +20,32 @@ class Block:
         return sha.hexdigest()
 
 
-def main():
+def test1():
     block_1 = Block(datetime.datetime.now(), "test data 1", None)
-    block_2 = Block(datetime.datetime.now(), "test data 2", block_1)
-    block_3 = Block(datetime.datetime.now(), "test data 3", block_2)
+    block_2 = Block(datetime.datetime.now(), "test data 2", block_1.hash)
+
+    print(block_1.hash == block_2.previous_hash)
+    # True
+
+
+def test2():
+    block_1 = Block(datetime.datetime.now(), "test data 1", None)
+    block_2 = Block(datetime.datetime.now(), "test data 2", block_1.hash)
+    block_3 = Block(datetime.datetime.now(), "test data 3", block_2.hash)
+
+    print(block_1.hash == block_2.previous_hash)
+    # True
+    print(block_2.hash == block_3.previous_hash)
+    # True
+
+
+def test3():
+    block_1 = Block(datetime.datetime.now(), "test data 1", None)
 
     print(block_1.hash)
-    print(block_2.hash)
-    print(block_3.hash)
+    # 05e8fdb3598f91bcc3ce41a196e587b4592c8cdfc371c217274bfda2d24b1b4e
 
 
-main()
+test1()
+test2()
+test3()
---
+++ 2_data_structures/p5_blockchain/readme.md
@@ -0,0 +1,2 @@
+The significance in the problem is the block made later is linked to the
+previous block using hash value.
