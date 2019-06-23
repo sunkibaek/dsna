955485862f2ca77cf7c6d66da8553139a5d1d621
Sunki Baek <sunki.baek@gmail.com>
2019-06-22 18:44:37 -0700

    Add solution to rearrange

--- 3_basic_algorithms/p3_rearrange_digits/rearrange_digits.py
+++ 3_basic_algorithms/p3_rearrange_digits/rearrange_digits.py
@@ -7,6 +7,17 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
+    sorted_list = sorted(input_list, reverse=True)
+    odd = []
+    even = []
+
+    for index, element in enumerate(sorted_list):
+        if index % 2:
+            even.append(str(element))
+        else:
+            odd.append(str(element))
+
+    return [int("".join(odd)), int("".join(even))]
 
 
 def test_function(test_case):
@@ -19,4 +30,4 @@ def test_function(test_case):
 
 
 test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
