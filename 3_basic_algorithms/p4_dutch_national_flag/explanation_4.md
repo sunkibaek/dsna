a0168b99cf72ea9932394bca6a647f9d29b8fb39
Sunki Baek <sunki.baek@gmail.com>
2019-07-11 23:29:05 -0700

    Add explanation

---
+++ 3_basic_algorithms/p4_dutch_national_flag/explanation_4.md
@@ -0,0 +1,3 @@
+To sort numbers in one traversal, we can fill up the result array from left for 0
+and from right for 2. For 1s, we can remember the number of 0s and use that
+information to store 1s after 0s.
--- 3_basic_algorithms/p4_dutch_national_flag/sort_012.py
+++ 3_basic_algorithms/p4_dutch_national_flag/sort_012.py
@@ -13,16 +13,20 @@ def sort_012(input_list):
     }
 
     for index, element in enumerate(input_list):
+        # fill the list from left for 0
         if element == 0:
+            # if 0 spill into 1 move 1 to right by 1
             if sorted_list[counts[0]] == 1:
                 sorted_list[counts[0]] = 0
                 sorted_list[counts[0] + counts[1]] = 1
             else:
                 sorted_list[counts[0]] = 0
 
+        # fill the list after 0 for 1
         if element == 1:
             sorted_list[counts[0] + counts[1]] = 1
 
+        # fill the list from right for 2
         if element == 2:
             sorted_list[len(input_list) - 1 - counts[2]] = 2
 
@@ -33,14 +37,25 @@ def sort_012(input_list):
 
 def test_function(test_case):
     sorted_array = sort_012(test_case)
-    print(sorted_array)
+
     if sorted_array == sorted(test_case):
         print("Pass")
     else:
-        print("Fail")
+        print("Fail", sorted_array)
 
 
+# regular case
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+
+# regular case
+test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
+
+# regular case
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2,
                2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
+
+# already sorted
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+
+# minimum case
+test_function([2, 1, 0])
