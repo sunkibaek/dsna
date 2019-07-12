a871cb1d1bb4060eac0be16ef0dcb8722a605ae6
Sunki Baek <sunki.baek@gmail.com>
2019-07-12 00:09:18 -0700

    Add explanation

---
+++ 3_basic_algorithms/p6_unsorted_integer_array/explanation_6.md
@@ -0,0 +1,2 @@
+Iterate the list to find min and max. To determine min see if the element is
+smaller than saved min. The same logic applies to max.
--- 3_basic_algorithms/p6_unsorted_integer_array/get_min_max.py
+++ 3_basic_algorithms/p6_unsorted_integer_array/get_min_max.py
@@ -11,6 +11,7 @@ def get_min_max(ints):
     min = None
     max = None
 
+    # iterating the list in O(n)
     for i in ints:
         if min == None or min > i:
             min = i
@@ -23,7 +24,18 @@ def get_min_max(ints):
 # Example Test Case of Ten Integers
 
 
+def test_function(actual, expect):
+    if actual[0] == expect[0] and actual[1] == expect[1]:
+        print("Pass")
+    else:
+        print("Fail", actual, expect)
+
+
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
-print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+test_function(get_min_max(l), (0, 9))
+
+test_function(get_min_max([0, 1]), (0, 1))
+
+test_function(get_min_max([1, 9]), (1, 9))
